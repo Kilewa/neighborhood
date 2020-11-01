718d5ab635874b0274fa172500b3894a0d8c3b02
kilewa <kilewageorge230@gmail.com>
2020-11-02 00:13:29 +0300

    created views for index page

---
+++ neighbourhoods/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import *
+
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        exclude = ['user','location']
+        
+class PostsForm(forms.ModelForm):
+    class Meta:
+        model = Posts
+        exclude = ['user','location']
+        
+
+    
--- neighbourhoods/views.py
+++ neighbourhoods/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views.generic import TemplateView
+from .models import Posts,Profile,Neighbourhood,Business
+from .forms import BusinessForm,PostsForm
+from django.contrib.auth.models import User
 
 class HomePage(TemplateView):
     template_name = 'neighbourhoods/index.html'
+
+
+def index(request):
+    hoods = Neighbourhood.objects.all()
+    
+    return render(request,'index.html',{"hoods":hoods})
+
